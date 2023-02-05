65a06ac48fe160b8211f68baff71a48d3f8ae237
Felix Nzioki Savali <felixnzioki99@gmail.com>
2023-02-05 19:22:27 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -5,7 +5,7 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front);
 int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
- * get_builtin - Matches a command with a corresponding
+ * get_buin - Matches a command with a corresponding
  *               shellby builtin function.
  * @command: The command to match.
  *
